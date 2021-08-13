410b7059c2c40f81208e6dee9054d9899a429cf3
Arunram07 <81552496+Arunram07@users.noreply.github.com>
2021-08-13 15:22:02 +0530

    Fix : daily rewards model

--- models/daily.js
+++ models/daily.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const rewardsSchema = mongoose.Schema({
-  dailyId: { type: Number },
   user: { type: String },
   earnings: { type: Number, default: 0 },
   userReferrers: { type: [String], default: [] },
